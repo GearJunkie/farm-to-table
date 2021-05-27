dded282730105009d1de3ae2463fcf0f9a6d55e2
jesse dills <jessedills@yahoo.com>
2021-05-27 08:07:11 -0400

    created prompts and color

---
+++ app.js
@@ -0,0 +1,13 @@
+let userName = prompt("Please Enter Your Email: ");
+console.log(userName);
+document.write("<h2>" + userName + "</h2>");
+
+let userAnswer = prompt("Would you like to sign up for Farm to Table?");
+console.log(userAnswer);
+
+if (userAnswer == "yes") {
+    document.write("<h2>We will send you an email with a link to register for your account!</h3>");
+
+} else {
+    document.write("<h2>Support Your Local Farmer's Market!<h2>");
+}
