229e5fce32f211aa3d0be039cb4ea970e67a3831
jesse dills <jessedills@yahoo.com>
2021-05-27 15:57:14 -0400

    added functions

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let userName = prompt("Please Enter Your Email: ");
-console.log(userName);
-document.write("<h2>" + userName + "</h2>");
+function userName(){
+    let userName = prompt("Please Enter Your Email: ");
+    console.log(userName);
+    document.write("<h2>" + userName + "</h2>");
+    return userName;
+}
 
-let userAnswer = prompt("Would you like to sign up for Farm to Table?");
-console.log(userAnswer);
+function userAnswer(){
+    let userAnswer = prompt("Would you like to sign up for Farm to Table?")
+    console.log(userAnswer);
 
-if (userAnswer == "yes") {
-    document.write("<h2>We will send you an email with a link to register for your account!</h3>");
+    if (userAnswer == "yes") {
+        console.log(userName);
+        document.write("<h2>We will send you an email with a link to register for your account!</h3>");
 
-} else {
-    document.write("<h2>Support Your Local Farmer's Market!<h2>");
+    } else {
+        document.write("<h2>Support Your Local Farmer's Market!<h2>");
+        return userName;
+    }
 }
